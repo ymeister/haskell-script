309069fc36c5c8e7e993b440db4080f67e5ccd40
ymeister <47071325+ymeister@users.noreply.github.com>
2026-07-13 01:39:30 +0900

    Pass default-extensions as -X flags to script and repl
    Add a default-extensions list to default.nix and interpolate it as
    -X<ext> --opts into both the haskell-script and haskell-repl wrappers.

--- default.nix
+++ default.nix
@@ -13,6 +13,98 @@ let nix-shebang-src = inputs.nix-shebang or ./deps/nix-shebang;
     #
     base = nix-shebang.haskell.nix-haskell;
     prelude-overlay = haskell-prelude-src + "/overlay/nix-haskell.nix";
+    #
+    default-extensions = [
+      "GHC2024"
+      "ApplicativeDo"
+      "Arrows"
+      "BangPatterns"
+      "BinaryLiterals"
+      "BlockArguments"
+      "ConstrainedClassMethods"
+      "ConstraintKinds"
+      "DataKinds"
+      "DefaultSignatures"
+      "DeriveDataTypeable"
+      "DeriveFoldable"
+      "DeriveFunctor"
+      "DeriveGeneric"
+      "DeriveLift"
+      "DeriveTraversable"
+      "DerivingStrategies"
+      "DerivingVia"
+      "DisambiguateRecordFields"
+      "DoAndIfThenElse"
+      "DuplicateRecordFields"
+      "EmptyCase"
+      "EmptyDataDecls"
+      "EmptyDataDeriving"
+      "ExistentialQuantification"
+      "ExplicitForAll"
+      "ExplicitNamespaces"
+      "ExtendedDefaultRules"
+      "FlexibleContexts"
+      "FlexibleInstances"
+      "FunctionalDependencies"
+      "GADTs"
+      "GeneralizedNewtypeDeriving"
+      "HexFloatLiterals"
+      "ImportQualifiedPost"
+      "ImpredicativeTypes"
+      "InstanceSigs"
+      "KindSignatures"
+      "LambdaCase"
+      "LexicalNegation"
+      "LiberalTypeSynonyms"
+      "MonadComprehensions"
+      "MultilineStrings"
+      "MultiParamTypeClasses"
+      "MultiWayIf"
+      "NamedDefaults"
+      "NegativeLiterals"
+      "NoAllowAmbiguousTypes"
+      "NoDeriveAnyClass"
+      "NoFieldSelectors"
+      "NoNamedFieldPuns"
+      "NoRecordWildCards"
+      "NumericUnderscores"
+      "OrPatterns"
+      "OverloadedLabels"
+      "OverloadedLists"
+      "OverloadedRecordDot"
+      "OverloadedRecordUpdate"
+      "OverloadedStrings"
+      "PackageImports"
+      "ParallelListComp"
+      "PartialTypeSignatures"
+      "PatternGuards"
+      "PatternSynonyms"
+      "PolyKinds"
+      "PostfixOperators"
+      "QualifiedDo"
+      "QuantifiedConstraints"
+      "QuasiQuotes"
+      "RankNTypes"
+      "RecursiveDo"
+      "RequiredTypeArguments"
+      "RoleAnnotations"
+      "ScopedTypeVariables"
+      "StandaloneDeriving"
+      "StandaloneKindSignatures"
+      "TemplateHaskell"
+      "TemplateHaskellQuotes"
+      "TransformListComp"
+      "TupleSections"
+      "TypeApplications"
+      "TypeData"
+      "TypeFamilies"
+      "TypeFamilyDependencies"
+      "TypeOperators"
+      "TypeSynonymInstances"
+      "UndecidableInstances"
+      "UnicodeSyntax"
+      "ViewPatterns"
+    ];
 
 in symlinkJoin {
   name = "haskell-script";
@@ -21,14 +113,23 @@ in symlinkJoin {
       writeScriptBin "haskell-script" ''
         #!/bin/sh
 
-        exec "${base}/bin/nix-haskell-shebang" --opts -O2 -threaded -rtsopts -with-rtsopts=-N --deps shh prelude --module '(import ${prelude-overlay})' "$@"
+        exec "${base}/bin/nix-haskell-shebang" \
+          --opts -O2 -threaded -rtsopts -with-rtsopts=-N \
+          --opts ${lib.concatMapStringsSep " " (ext: "-X${ext}") default-extensions} \
+          --deps shh prelude \
+          --module '(import ${prelude-overlay})' \
+          "$@"
       ''
     )
     (
       writeScriptBin "haskell-repl" ''
         #!/bin/sh
 
-        exec "${base}/bin/nix-haskell-repl" --deps shh prelude --module '(import ${prelude-overlay})' "$@"
+        exec "${base}/bin/nix-haskell-repl" \
+          --opts ${lib.concatMapStringsSep " " (ext: "-X${ext}") default-extensions} \
+          --deps shh prelude \
+          --module '(import ${prelude-overlay})' \
+          "$@"
       ''
     )
   ];
