d9389b758294e7592b6e294954b1c9acdb3eb384
ymeister <47071325+ymeister@users.noreply.github.com>
2026-07-13 02:12:18 +0900

    Cleanup

--- test/test.hs
+++ test/test.hs
@@ -1,5 +1,5 @@
 #!/usr/bin/env nix
-#!nix shell --no-write-lock-file .?submodules=1#default --command sh -c ``haskell-script -- "$@"`` sh
+#!nix shell .?submodules=1#default --command sh -c ``haskell-script -- "$@"`` sh
 
 {-# LANGUAGE NoImplicitPrelude #-}
 
